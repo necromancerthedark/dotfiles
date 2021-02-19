85024a349c0a51bca53b4e350261115191c3ff17
necromancerthedark <kg22584@gmail.com>
2021-02-19 16:11:56 +0530

    kali files added

--- full.sh
+++ full.sh
@@ -1,5 +1,5 @@
 xrandr --newmode "1280x1080_60.00" 1720.80 1920 2040 2248 2576 1080 1081 1084 1118 -HSync +VSync 
 
-xrandr --addmode Virtual1 "1280x1080_60.00"
+xrandr --addmode Virtual-1 "1280x1080_60.00"
 
-xrandr --output Virtual1 --mode "1280x1080_60.00"
+xrandr --output Virtual-1 --mode "1280x1080_60.00"
